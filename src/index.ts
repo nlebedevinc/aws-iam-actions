7031c696998d204166f47f3940fba4da6584145c
nlebedevinc <nlebedevme@gmail.com>
2021-08-01 17:44:28 +0200

    make the tool warking

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,55 @@
 
 import { program } from 'commander';
 
-// requester class
+import https from 'https';
+
+const IAM_ACTIONS = 'https://awspolicygen.s3.amazonaws.com/js/policies.js';
+
+function normalize(raw: string): string {
+    return raw.substring(raw.indexOf("=") + 1);
+}
+
+// requester
+async function request(): Promise<AWSActionData> {
+    return new Promise((resolve) => {
+        https.get(IAM_ACTIONS, res => {
+            let data: Array<Uint8Array> = [];
+    
+            res.on('data', chunk => {
+                data.push(chunk);
+            });
+    
+            res.on('end', () => {
+                const temp = Buffer.concat(data).toString();
+                const normalized = normalize(temp);
+                resolve(JSON.parse(normalized));
+            });
+        });
+    });
+}
+
+interface ActionContent {
+    StringPrefix: string;
+    Actions: Array<string>;
+    ARNformat: string;
+    ARNregex: string;
+}
+
+type Response = { [name: string]: ActionContent };
+type AWSActionData = { serviceMap: Response };
+
+function find(query: string, data: AWSActionData): Response | null {
+    const { serviceMap } = data;
+
+    for (let key in serviceMap) {
+        const action = serviceMap[key];
+        if (serviceMap[key].StringPrefix === query) {
+            return { key: action };
+        }
+    }
+
+    return null;
+}
 
 // validate process vars
 
@@ -20,7 +68,15 @@ async function main(): Promise<void> {
         throw new Error('Please provice AWS service name');
     }
 
-    console.info('success', serviceName);
+    const actions = await request();
+    const result = find(serviceName, actions);
+
+    if (!result) {
+        console.info(`AWS service ${serviceName} does not exist`);
+        process.exit(0);
+    }
+
+    console.info(result);
 }
 
 main().catch(error => {
